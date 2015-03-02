722c7e5bba53a6004e08d31341509cbc8d2662b2
unknown <icarr@7440W7ICARR.extendthereach.com>
2015-03-02 10:09:40 -0700

    Initial commit of basic batch script to add jobs to the Alteryx Service queue.

---
+++ runjob.bat
@@ -0,0 +1,44 @@
+@echo off
+
+REM SERVER SETTINGS
+set "host=localhost:80"
+set "secret=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+set "alteryx_service=C:\Program Files\Alteryx\bin\alteryxservice.exe"
+
+REM FILE SETTINGS
+set "working_dir=C:\temp"
+set "work_module=work.yxmd"
+set "last_module=last.yxmd"
+set "last_file=done.csv"
+
+REM NUMBER OF JOBS TO ADD TO QUEUE
+set jobs=25
+
+
+set STARTTIME=%TIME%
+set num=1
+
+:addjobtoqueue
+IF %num% LEQ %jobs% (
+    ECHO adding job (%num%^) to queue
+    "%alteryx_service%" addtoqueue="%working_dir%\%work_module%",%host%,%secret%
+    set /a num+=1
+    GOTO addjobtoqueue
+)
+
+rem add a job to the queue that creates a temp file indicating that we are finished (done.csv)
+"%alteryx_service%" addtoqueue="%working_dir%\%last_module%",%host%,%secret%
+
+:waittilldone
+IF NOT EXIST %working_dir%\done.csv (
+TIMEOUT /t 1 /nobreak
+GOTO waittilldone
+)
+
+set ENDTIME=%TIME%
+
+rem delete the temp file created by the last job
+del "%working_dir%\%last_file%"
+
+echo START: %STARTTIME%
+echo END:   %ENDTIME%
